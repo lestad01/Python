1947bd1cdbcae45282fcfacf65b66e223f92ed07
Aleksander Chernov <lestad01@mail.ru>
2024-06-12 20:41:15 +0300

    12.06.24

---
+++ course_python_24year/1_python.py
@@ -0,0 +1,11 @@
+# создание директолрии python -m venv my_venv
+# удаление директории  rm myenv/
+# удаление рекурсивно rm -r my_env
+# cat my_env/bin/activate
+# head 
+a = 1
+
+if a > 2:
+    print(f"num {a} is grether than 3")
+else:
+    print(f"num {a} is less than 2")
